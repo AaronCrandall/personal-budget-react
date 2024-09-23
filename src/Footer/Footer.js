4788c408b8e642123cb2a176fde3a09465fc28e2
Aaron Crandall <117679437+AaronCrandall@users.noreply.github.com>
2024-09-23 11:12:55 -0400

    Video done

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer class="bottom">
+        <section class="center">
+            All rights reserved &copy; Fabio Nolasco
+        </section>
+    </footer>
+  );
+}
+
+export default Footer;
